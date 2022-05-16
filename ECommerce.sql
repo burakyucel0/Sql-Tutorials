96bcdf87b5dfb458621df379b2bf630bcc4ee7b3
BurakYücel <56517864+burakyucel0@users.noreply.github.com>
2022-05-16 17:24:42 +0300

    E-Commerce Database Design

---
+++ ECommerce.sql
@@ -0,0 +1,59 @@
+create table User (
+ID int PRIMARY KEY IDENTITY (1,1),
+FirstName NVARCHAR (MAX),
+LastName NVARCHAR (MAX),
+ADDRESSID int,
+MembershipDate datetime
+)
+
+create table Product (
+ID int PRIMARY KEY IDENTITY (1,1),
+Rank decimal,
+Weight decimal,
+PublshDate Date,
+Price Money
+)
+
+create table Category (
+TOPID int PRIMARY KEY IDENTITY (1,1),
+ID int UNIQUE ,
+CategoryName NVARCHAR (MAX)
+)
+
+create table ProductCategory (
+ProductID int,
+CategoryID int
+)
+
+Create table Favorite (
+UserID int,
+ProductID int
+)
+
+create table ProductCart (
+ID int PRIMARY KEY IDENTITY (1,1),
+ProductID int,
+CartPrice money
+)
+
+create table Order (
+ID int PRIMARY KEY IDENTITY (1,1),
+ProductCartID int,
+TotalPrice money
+)
+
+create table City (
+ID int PRIMARY KEY IDENTITY (1,1),
+Name NVARCHAR (MAX),
+)
+create table District (
+ID int PRIMARY KEY IDENTITY (1,1),
+Name NVARCHAR (MAX),
+)
+
+create table Address (
+ID int PRIMARY KEY IDENTITY (1,1),
+CityID int,
+DistrictID int,
+OpenAddress NVARCHAR (MAX),
+)
